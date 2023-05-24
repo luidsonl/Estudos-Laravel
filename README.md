b46367fae5e0cca942e959c4c1057198f98d53be
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-05-24 18:35:16 -0300

    feat: creating route, controller and view to /support

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # E-commerce simples feito com Laravel
 
 [:file_folder:](./e-commerce)
-
-<a href='https://www.youtube.com/watch?v=6R5hMKAW7n4'> Base </a>
+<a href='https://www.youtube.com/watch?v=6R5hMKAW7n4'> Tutorial </a>
 
 # Curso gratuito de Laravel 10.x
 
 [:file_folder:](./curso-gratuito-laravel-10x)
-
-<a href = 'https://www.youtube.com/playlist?list=PLVSNL1PHDWvQ1N6fqhQ5HQzFtN-xrkjNU'>Base</a>
+<a href = 'https://www.youtube.com/playlist?list=PLVSNL1PHDWvQ1N6fqhQ5HQzFtN-xrkjNU'>Curso</a>
--- curso-gratuito-laravel-10x/routes/web.php
+++ curso-gratuito-laravel-10x/routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\{SupportController};
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SiteController;
+use App\Http\Controllers\Site\SiteController;
+//Route::get('/contact', [SiteController::class, 'contact']);
+//A rota é definida para a URL '/contact'. Quando um usuário acessa essa URL no navegador, a função 'contact' do controller 'SiteController' será executada.
 
 Route::get('/contact', [SiteController::class, 'contact']);
-//A rota é definida para a URL '/contact'. Quando um usuário acessa essa URL no navegador, a função 'contact' do controller 'SiteController' será executada.
+Route::get('/supports', [SupportController::class, 'index'])->name('/supports.index');
 
 Route::get('/', function () {
     return view('welcome');
