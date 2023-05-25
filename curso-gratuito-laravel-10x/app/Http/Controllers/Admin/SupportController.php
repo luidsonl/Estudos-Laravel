85a9840f0bdd302388cd8775a50c014d4aacad22
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-05-24 22:27:29 -0300

    feat: model and miration - support

--- curso-gratuito-laravel-10x/app/Http/Controllers/Admin/SupportController.php
+++ curso-gratuito-laravel-10x/app/Http/Controllers/Admin/SupportController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index(){
-        return view('admin/supports/index');
+    public function index(Support $support){
+
+        $supports = $support->all();
+        dd($supports);
+
+        return view('admin/supports/index', [
+            'supports'=>$supports
+        ]);
     }
 }
