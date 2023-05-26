faea4d935dad1e3dfc121328109321d00542bf55
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-05-25 21:18:47 -0300

    fix: supports view

--- curso-gratuito-laravel-10x/resources/views/admin/supports/index.blade.php
+++ curso-gratuito-laravel-10x/resources/views/admin/supports/index.blade.php
@@ -1,23 +1,28 @@
 @extends('master')
 @section('content')
-    
-<h1>Listagem dos supports</h1>
 
-<table>
-    <thead>
-        <th>Assunto</th>
-        <th>Descrição</th>
-        <th>Status</th>
-    </thead>
-    <tbody>
-        @foreach ($supports as $support)
-            <tr>
-                <td> {{ $support->subject}} </td>
-                <td> {{ $support->body}} </td>
-                <td> {{ $support->status}} </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+<main class="container">
+    <h1 >Listagem dos supports</h1>
+
+    <table class="table">
+        <thead class="">
+            <th>Assunto</th>
+            <th>Descrição</th>
+            <th>Status</th>
+            <th></th>
+        </thead>
+        <tbody>
+            @foreach ($supports as $support)
+                <tr>
+                    <td> {{ $support->subject}} </td>
+                    <td> {{ $support->body}} </td>
+                    <td> {{ $support->status}} </td>
+                    <td></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</main>
+
 
 @endsection
